92153a7531c7c4dad2ed1b89875bd4f64c824330
seyyar95 <heydrovsyyr@gmail.com>
2023-12-26 02:57:53 -0800

    env

--- shell.c
+++ shell.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-
+#include "main.h"
 
 /**
  * read_command - Reads a command from the user and returns it as a string.
