4ba504ed170184d658677c4dddaacd08d28be72d
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-12-29 05:16:26 -0800

    Updated betty style

--- shell.c
+++ shell.c
@@ -1,5 +1,14 @@
 
+
 #include "main.h"
+
+void execute_external_command(char **args);
+void execute_command_child(char **args, char *command);
+int execute_command(char *command);
+void execute_env_command(void);
+void execute_absolute_path_command(char **args);
+void wait_and_free(char *command, int *status);
+
 
 /**
  * read_command - reads command
@@ -47,15 +56,54 @@ void parse_arguments(char *command, char **args)
 	args[arg_count] = NULL;
 }
 
-/**
- * execute_command - executes command
- *
- * 
- *@command: The command string to execute.
- *
- * Return: the exit status of the executed command,
- * or -1 if an error occurs.
- */
+
+
+
+void execute_external_command(char **args)
+{
+	char *path = getenv("PATH");
+	char *token;
+
+	if (path == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		exit(127);
+	}
+	token = strtok(path, ":");
+
+	while (token != NULL)
+	{
+		char executable_path[256];
+
+		snprintf(executable_path, sizeof(executable_path), "%s/%s", token, args[0]);
+		if (access(executable_path, X_OK) == 0)
+		{
+			if (execve(executable_path, args, environ) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, ":");
+	}
+	fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+	exit(127);
+}
+
+void execute_command_child(char **args, char *command)
+{
+	if (args[0] == NULL)
+	{
+		free(command);
+		exit(EXIT_SUCCESS);
+	}
+	if (strcmp(args[0], "env") == 0)
+		execute_env_command();
+	if (strchr(args[0], '/') != NULL)
+		execute_absolute_path_command(args);
+	else
+		execute_external_command(args);
+}
 
 int execute_command(char *command)
 {
@@ -73,81 +121,52 @@ int execute_command(char *command)
 		char *args[64];
 
 		parse_arguments(command, args);
-		if (args[0] == NULL)
-		{
-			free(command);
-			exit(EXIT_SUCCESS);
-		}
-		if (strcmp(args[0], "env") == 0)
-		{
-			char **env = environ;
-
-			while (*env != NULL)
-			{
-				printf("%s\n", *env);
-				env++;
-			}
-			free(command);
-			exit(EXIT_SUCCESS);
-		}
-		if (strchr(args[0], '/') != NULL)
-		{
-			if (access(args[0], X_OK) == 0)
-			{
-				if (execve(args[0], args, environ) == -1)
-				{
-					perror("execve");
-					free(command);
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
-		else
-		{
-			char *path = getenv("PATH");
-			char *token;
-
-			if (path == NULL)
-			{
-				fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-				free(command);
-				exit(127);
-			}
-			token = strtok(path, ":");
-
-			while (token != NULL)
-			{
-				char executable_path[256];
-
-				snprintf(executable_path, sizeof(executable_path), "%s/%s", token, args[0]);
-				if (access(executable_path, X_OK) == 0)
-				{
-					if (execve(executable_path, args, environ) == -1)
-					{
-						perror("execve");
-						free(command);
-						exit(EXIT_FAILURE);
-					}
-				}
-
-				token = strtok(NULL, ":");
-			}
-		}
-		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-		free(command);
-		exit(127);
+		execute_command_child(args, command);
 	}
 	else
-	{
-		waitpid(pid, &status, 0);
-		free(command);
-		if (WIFEXITED(status))
-			status = WEXITSTATUS(status);
-		else
-			status = 1;
-	}
+		 wait_and_free(command, &status);
 	return (status);
 }
+
+void execute_env_command(void)
+{
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+	exit(EXIT_SUCCESS);
+}
+
+void execute_absolute_path_command(char **args)
+{
+	if (access(args[0], X_OK) == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	execute_external_command(args);
+}
+
+void wait_and_free(char *command, int *status)
+{
+	pid_t pid = waitpid(-1, status, 0);
+
+	free(command);
+	if (WIFEXITED(*status))
+		*status = WEXITSTATUS(*status);
+	else
+		*status = 1;
+}
+
+
+
+
 
 /**
  * main - Entry poin
